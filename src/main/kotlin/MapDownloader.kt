f98cfa9033a0333c683db6279b465d5735103aef
Bktero <plgbasura@gmail.com>
2025-03-18 14:06:34 +0100

    Check if the download dir is really created

--- src/main/kotlin/MapDownloader.kt
+++ src/main/kotlin/MapDownloader.kt
@@ -1,12 +1,31 @@
 import data.Map
 import java.io.File
 import java.net.URI
+import kotlin.system.exitProcess
 
 class MapDownloader(private val directory: File) {
 
     init {
         // Make sure the directory exists; otherwise, creating files will fail
-        directory.mkdirs()
+        println("Creating directory $directory...")
+        val created = directory.mkdirs()
+
+        // FIXME it seems that (at least once on the VPS) the "working" dir may be created
+        //  but not the "downloads" dir
+        if (created) {
+            println("OK")
+        } else {
+            println("Failure")
+            exitProcess(1)
+        }
+
+        // Check if the directory exists
+        if (directory.exists() && directory.isDirectory) {
+            println("Directory exists and is a directory")
+        } else {
+            println("Directory does not exist or is not a directory")
+            exitProcess(1)
+        }
     }
 
     fun download(map: Map): File {
